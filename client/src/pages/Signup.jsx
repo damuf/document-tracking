f952209dc01f9a90ecf86ce3ac46c6543c8d1790
damuf <umanadaii@gmail.com>
2022-06-18 23:12:52 -0600

    signin view a medias

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import SignupForm from '../components/SignupForm/SignupForm'
+import Footer from '../components/Footer/Footer'
+import './Signin.css'
+
+function Signin() {
+  return (
+    <div>
+       <SignupForm/>
+       <Footer/>
+    </div>
+  )
+}
+
+export default Signin
