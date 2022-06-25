833249c0f4db1527572f9ab03d2bd0ec50ae19d4
Robotooo <50878739+Robotooo@users.noreply.github.com>
2022-06-25 01:12:29 -0600

    genera codigo alfanumerico

--- client/src/components/Navbar/Documentos.jsx
+++ client/src/components/Navbar/Documentos.jsx
@@ -1,9 +1,35 @@
-function Documentos() {
+import React from "react";
+
+function Documentos(){
+
+  const onSubmit = event => {
+    event.preventDefault();
+    CrearCodAlphanumerico();
+  };
+
+
+  function CrearCodAlphanumerico(){ //genera el codigo alphanumerico
+    let jeje = document.getElementById("aronou").value; //agrega la palabra que se ingrese en el input de abajo
+    var text = "";
+    text += jeje + "-" ; //pa poner el guion que separa jeje
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 30; i++){ //cantidad de letras y numeros agregados aleatoriamente
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    console.log("Alphanumerico: "+text);
+    return text;
+  }
+
   return (
     <div className="containerNavComponents">
-      <h1>Documentos</h1>
+      <h1>Generar numeros Alfanumericos</h1>
+      <form onSubmit={onSubmit}>
+      <input type="text" className="navInput" id="aronou" placeholder="Digite 3 letras" maxLength="3" minLength="3" pattern="[A-Z,a-z]{3}" ></input><br/><br/>
+      <button className="buttonStart" type="submit" >Generar</button>
+      </form>
     </div>
   );
+
 }
 
 export default Documentos;
--- server/src/model/casos.js
+++ server/src/model/casos.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const casosSchema = new Schema({
     idTramite: {ref:"tramites", type: Schema.Types.ObjectId, required: true},
-    numCaso: {type: Number, required: true, unique: true}, 
+    numCaso: {type: String, required: true, unique: true}, 
     fechaApertura: {type: Date, required: true},
     fechaFinal: {type: Date, required: false},
     estado: {type: String, required: true},
