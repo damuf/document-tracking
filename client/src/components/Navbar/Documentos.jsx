888035c6a239bb467fab479715138a7ea77039a6
Robotooo <50878739+Robotooo@users.noreply.github.com>
2022-06-26 21:29:33 -0600

    departamentos dinamico

--- client/src/components/Navbar/Documentos.jsx
+++ client/src/components/Navbar/Documentos.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Component } from 'react';
 import axios from 'axios';
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import {  useNavigate } from "react-router-dom";
 import Error from '../Alerts/Error'
 import Success from '../Alerts/Success'
 import pic from "../../assets/bald-man.png";
 import './NavbarComponents.css'
 
-function Documentos(){
+
+
+export default function Documentos(){
 
     //alerts
     const [isError, setIsError] = useState(false);
@@ -21,7 +23,8 @@ function Documentos(){
     const [estado, setEstado] = useState('Activo');
     const [departamentos, setDepartamentos] = useState('');
     const [orden, setOrden] = useState('');
-
+    const [data, setData] = useState([])
+    const [depas, setDepas] = useState([{}])
 
     const showError = () => {
       setTimeout( () => {
@@ -29,30 +32,49 @@ function Documentos(){
       }, 5000);
     }
 
+    
   const onSubmit = async(event)=>{
     event.preventDefault();
     try{
-      const {data} = await axios.post(`localhost:4000/casos/numAlpha`)
-    }catch(error) {
-      setIsError(true)
-      showError()
-      setMessage(error.response.data.message)
-    }
+        //  axios.get(`http://localhost:4000/departamentos`).then(response => { 
+        //     const jeje = response.data;
+        //     this.setDepartamentos({jeje})
+        // });
+    
+        const {data: response} = await axios.get(`http://localhost:4000/departamentos`);
+            setData(response);
+        setDepas(response)
+    
+    
+        }catch(error) {
+        //   setIsError(true)
+        //   showError()
+        //   setMessage(error.response.data.message)
+    
+          console.error(error.message);
+        }
   }
-  const olabb = async() => {
 
-    console.log("JEJEJ: "+estado);
+  const departamentosCarga = async () => {
+    try{
+        const {data: response} = await axios.get(`http://localhost:4000/departamentos`);
+        setData(response);
+        setDepas(response)
+        }catch(error) {
+          console.error(error.message);
+        }
   }
-
-
 
   return (
     <>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-    <div className="containerStart" style={{userSelect: "none"}}>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+
+    <div className="containerStart" style={{userSelect: "none"}} onChange={departamentosCarga()}>
 
     <div className="frow">
         {isError && <Error msg={message}/>}
@@ -67,52 +89,80 @@ function Documentos(){
         <div id="form" className="fcolumn" style={{width: "520px"}}>
         <h1 style={{textShadow: 'red -2px 0, cyan 2px 0'}}>Crear caso</h1>
 
-        <form id="sigupform" method="get" onSubmit={onSubmit}>
+        <form id="departametosform" method="get" onSubmit={onSubmit}>
 
             <div className="frow">
                 <div className="frow" style={{marginRight: '30px'}}>
-                    <i className="material-symbols-outlined">diversity_3</i>&nbsp;
+                    <i className="material-symbols-outlined">description</i>&nbsp;
                     <input type="text" id="tramite" placeholder="Numero de tramite" required={true} autoComplete="off" value={tramite} onChange={event => {setTramite(event.target.value)}}/>
                 </div> 
                 <div className="frow">    
-                    <i className="material-symbols-outlined">edit</i> &nbsp;
+                    <i className="material-symbols-outlined">barcode</i> &nbsp;
                     <input type="text" id="codAlfa" placeholder="Codigo"  required={true} autoComplete="off" value={numCaso} onChange={event => {setNumCaso(event.target.value)}} maxLength="3" minLength="3" pattern="[A-Z,a-z]{3}" ></input><br/><br/>
                 </div>
             </div>
             <br />
-            <div className="frow" >
-                <div className="frow" id="radios" style={{marginRight: '30px'}} value={setEstado} onChange={event => {setEstado(event.target.value)}}>
-                    <input id="radios" type="radio" value="Activo" name="gender" checked={true}/> Activo
-                    <input type="radio" value="Inactivo" name="gender" /> Inactivo
-                </div>
-                <div className="frow">
-                    <i className="material-symbols-outlined">diversity_3</i> &nbsp;
-                    <input type="text" id="departamentos" placeholder="Departamentos" required={true} autoComplete="off" value={departamentos} onChange={event => {setDepartamentos(event.target.value)}}/>
-                </div>
-            </div>
-            <br />
-
-            <br />
             <div className="frow" style={{display: '-webkit-inline-flex', marginLeft:'18px'}}>
                 <div className="frow" style={{width: '235px', marginLeft:'18px'}}>
-                    <i className="material-symbols-outlined">cake</i> &nbsp;
+                    <i className="material-symbols-outlined">calendar_month</i> &nbsp;
                     <input type="date" id="fApertura" placeholder="Fecha apertura" required={true} autoComplete="off" value={fechaApertura} onChange={event => {setFechaApertura(event.target.value)}}/>
                 </div>
                 <div className="frow" style={{width: '233px', marginLeft:'18px'}}>
-                    <i className="material-symbols-outlined">cake</i> &nbsp;
+                    <i className="material-symbols-outlined">calendar_month</i> &nbsp;
                     <input type="date" id="fFinal" placeholder="Fecha de cierre" required={true} autoComplete="off" value={fechaFinal} onChange={event => {setFechaFinal(event.target.value)}}/>
                 </div>
             </div>
             <br />
-                <div className="frow" style={{display: '-webkit-inline-flex', marginLeft:'18px'}}>
-                    Hola
+            <div className="frow" >
+            <br />
+                <h3 className='casosNav'>Estado</h3>
+                <br />
+                <div className="frow" id="radios" style={{marginRight: '30px'}} value={setEstado} onChange={event => {setEstado(event.target.value)}}>
+                    <br />
+                    <input id="radios" type="radio" value="Activo" name="gender" checked={true}/> Activo
+                    <input type="radio" value="Inactivo" name="gender" /> Inactivo
                 </div>
-                
+            </div>
+            <br />
+                <div className="frow" id="divChecks" style={{display: '-webkit-inline-flex', marginLeft:'18px'}}>
+                <div className='frow'>
+                    <i style={{marginRight: '10px'}} class="material-symbols-outlined">list</i>&nbsp;
+                    <h3 className='casosNav'>Departamentos</h3>
+                </div>
+                <div className='optionsCheckbox'>
+
+                    <ul className="">
+                        {depas.map(({ nombre }, index) => {
+                        return (
+                            <li key={index}>
+                            <div className="">
+                                <div className="checos">
+                                    <div className='fcolumn'>
+                                        <input
+                                            className='fcolumn'
+                                            type="checkbox"
+                                            id={`custom-checkbox-${index}`}
+                                            name={nombre}
+                                            value={nombre}
+                                            
+                                        />
+                                    </div>
+                                <label htmlFor={`custom-checkbox-${index}`} style={{ width: '100px', height: '45px'}}>{nombre}</label>
+                                </div>
+                            </div>
+                            </li>
+                        )
+                        })}
+                    </ul>
+                               
+                    </div>
+                </div>
+                <br />
                 <div className="frow" style={{display: '-webkit-inline-flex', marginLeft:'18px'}}>
                     hola
                 </div>
             <br/>
-            <button className="buttonStart" type="submit" onClick={olabb()}>Ola</button>
+            <button className="buttonStart" type="submit">Ola</button>
         </form>
         </div>
     </div>
@@ -124,4 +174,4 @@ function Documentos(){
 
 }
 
-export default Documentos;
+
--- server/src/model/documentos.js
+++ server/src/model/documentos.js
@@ -4,6 +4,8 @@ const documentosSchema = new Schema({
     idCaso: {ref:"casos", type: Schema.Types.ObjectId, required: true},
     idTramite: {ref:"tramites", type: Schema.Types.ObjectId, required: true},
     nombre: {type: String, required: true, lowercase: true},
+    estado: {type: String, required: true, lowercase: true},
+    medio: {type: String, required: true, lowercase: true},
     ruta: {type: String, required: true, lowercase: true}
 }, { versionKey: false })
 
